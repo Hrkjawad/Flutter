f17a3456fc91c88ea543a0f748741175052e8aa9
hrkjawad <hrkjawad10@gmail.com>
2023-12-15 23:24:40 +0600

    Mod16_ScoringAppWithCloudFirestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_task/MatchDetails.dart';
+import 'package:firebase_task/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
